7feeb3c6db56ec28ba24b455012ced3dabee177b
Matt Worzala <WorzalaM02@gmail.com>
2019-02-15 00:39:05 -0500

    sealed PrimitiveNode. Might remove the generic in the future, but it can stay for now.

--- src/main/kotlin/me/bhop/bcon/node/PrimitiveNode.kt
+++ src/main/kotlin/me/bhop/bcon/node/PrimitiveNode.kt
@@ -2,6 +2,13 @@ package me.bhop.bcon.node
 
 import kotlin.reflect.KClass
 
-open class PrimitiveNode<T : Any> internal constructor(id: String, comments: MutableList<String> = mutableListOf(), parent: Node?, val value: T) : Node(id, comments, parent) {
+sealed class PrimitiveNode<T : Any>(id: String, comments: MutableList<String> = mutableListOf(), parent: Node?, val value: T) : Node(id, comments, parent) {
     fun getType(): KClass<out T> = value::class
 }
+
+class StringNode(id: String, comments: MutableList<String> = mutableListOf(), parent: Node?, value: String) : PrimitiveNode<String>(id, comments, parent, value)
+class BooleanNode(id: String, comments: MutableList<String> = mutableListOf(), parent: Node?, value: Boolean) : PrimitiveNode<Boolean>(id, comments, parent, value)
+class IntNode(id: String, comments: MutableList<String> = mutableListOf(), parent: Node?, value: Int) : PrimitiveNode<Int>(id, comments, parent, value)
+class DoubleNode(id: String, comments: MutableList<String> = mutableListOf(), parent: Node?, value: Double) : PrimitiveNode<Double>(id, comments, parent, value)
+class LongNode(id: String, comments: MutableList<String> = mutableListOf(), parent: Node?, value: Long) : PrimitiveNode<Long>(id, comments, parent, value)
+class ShortNode(id: String, comments: MutableList<String> = mutableListOf(), parent: Node?, value: Short) : PrimitiveNode<Short>(id, comments, parent, value)
--- src/main/kotlin/me/bhop/bcon/test.kt
+++ src/main/kotlin/me/bhop/bcon/test.kt
@@ -3,7 +3,7 @@ package me.bhop.bcon
 import me.bhop.bcon.node.CategoryNode
 import me.bhop.bcon.node.ArrayNode
 import me.bhop.bcon.node.PrimitiveNode
-import me.bhop.bcon.node.primitive.StringNode
+import me.bhop.bcon.node.StringNode
 
 fun main(args: Array<String>) {
     val parent = CategoryNode("test", parent = null)
