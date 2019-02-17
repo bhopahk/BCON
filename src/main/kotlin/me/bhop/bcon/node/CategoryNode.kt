47c24e9eae790a77606d1ce3e300341a4bbffd71
Matt Worzala <WorzalaM02@gmail.com>
2019-02-17 01:49:37 -0500

    added method to detach a category from it's parent. CategoryNode -> OrphanNode

--- src/main/kotlin/me/bhop/bcon/node/CategoryNode.kt
+++ src/main/kotlin/me/bhop/bcon/node/CategoryNode.kt
@@ -2,4 +2,9 @@ package me.bhop.bcon.node
 
 class CategoryNode(id: String, comments: MutableList<String> = mutableListOf(), parent: Node, override var children: MutableList<Node> = mutableListOf()) : ChildNode(id, comments, parent), ParentNode {
     override fun asNode(): Node = this
+
+    fun toOrphanNode(): OrphanNode {
+        parent.getAsCategory()?.children?.remove(this)
+        return OrphanNode(children)
+    }
 }
--- src/main/kotlin/me/bhop/bcon/node/Node.kt
+++ src/main/kotlin/me/bhop/bcon/node/Node.kt
@@ -2,12 +2,12 @@ package me.bhop.bcon.node
 
 abstract class Node(val id: String, val comments: MutableList<String> = mutableListOf()) {
 
-    fun getAsCategory(): CategoryNode? = if (this is CategoryNode) this else null
+    fun getAsCategory(): ParentNode? = if (this is ParentNode) this else null
     fun getAsPrimitive(): PrimitiveNode? = if (this is PrimitiveNode) this else null
     fun getAsArray(): ArrayNode? = if (this is ArrayNode) this else null
 
     fun isOrphan(): Boolean = this is OrphanNode
-    fun isCategory(): Boolean = this is CategoryNode
+    fun isCategory(): Boolean = this is ParentNode
     fun isPrimitive(): Boolean = this is PrimitiveNode
     fun isArray(): Boolean = this is ArrayNode
 
